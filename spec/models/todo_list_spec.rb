16d1b82f825d5cb89e5dd4dfd3a5b4a4f1d7e2df
CraigKuriger <craig.kuriger@icloud.com>
2015-07-22 00:47:16 +1200

    functionality working

--- spec/models/todo_list_spec.rb
+++ spec/models/todo_list_spec.rb
@@ -2,4 +2,36 @@ require 'spec_helper'
 
 describe TodoList do
   it { should have_many(:todo_items)}
+
+  describe "has completed items?" do
+  	let(:todo_list) {TodoList.create(title: "Shopping", description: "Shopping List")}
+
+  	it "returns true with completed todo list items" do
+  		todo_list.todo_items.create(content: "Eggs", completed_at: 1.minute.ago)
+  		expect(todo_list.has_completed_items?).to be_truthy
+  	end
+
+  	it "returns false with no completed todo list items" do
+  		todo_list.todo_items.create(content: "Eggs")
+  		expect(todo_list.has_completed_items?).to be_falsey
+  	end
+
+  end
+
+  describe "has incompleted items?" do
+  	let(:todo_list) {TodoList.create(title: "Shopping", description: "Shopping List")}
+
+  	it "returns true with incompleted todo list items" do
+  		todo_list.todo_items.create(content: "Eggs")
+  		expect(todo_list.has_incomplete_items?).to be_truthy
+  	end
+
+  	it "returns tfalse with no completed todo list items" do
+  		todo_list.todo_items.create(content: "Eggs", completed_at: 1.minute.ago)
+  		expect(todo_list.has_incomplete_items?).to be_falsey
+  	end
+
+
+  end
+
 end
